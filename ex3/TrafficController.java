db38e6a8d05777f0adb33731826e23f9c91d08d0
rafaelmocorrea <rafaelmocorrea@gmail.com>
2021-11-22 18:32:05 -0300

    Exercicio 03 feito

--- ex3/TrafficController.java
+++ ex3/TrafficController.java
@@ -1,8 +1,28 @@
+import java.util.concurrent.Semaphore;
+
 public class TrafficController {
 
-    public void enterLeft() {}
-    public void enterRight() {}
-    public void leaveLeft() {}
-    public void leaveRight() {}
+    private static Semaphore sem = new Semaphore(1,true);
+
+    public void enterLeft() {
+        try {
+            this.sem.acquire();
+        } catch (InterruptedException e) {
+            System.out.println("Exception");
+        }
+    }
+    public void enterRight() {
+        try {
+            this.sem.acquire();
+        } catch (InterruptedException e) {
+            System.out.println("Exception");
+        }
+    }
+    public void leaveLeft() {
+        this.sem.release();
+    }
+    public void leaveRight() {
+        this.sem.release();
+    }
 
 }
